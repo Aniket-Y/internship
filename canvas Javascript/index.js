8a43b2ab25a1fb96366032c6339505ad38ac3bbb
Aniket Yeole <87656807+Aniket-Y@users.noreply.github.com>
2022-03-09 13:58:19 +0530

    Initial

---
+++ canvas Javascript/index.js
@@ -0,0 +1,43 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function rect() {
+  const gtc = canvas.getContext("2d");
+  gtc.clearRect(0, 0, canvas.width, canvas.height);
+  let input = document.querySelector("input");
+  let x = 10;
+  let y = 100;
+  let w = window.innerWidth / 9;
+  let h = window.innerHeight / 9;
+  for (let a = 0; a < input.value; a++) {
+    new Rectangle(x, y, w, h);
+    x += w + 20;
+    if (x > (w + 20) * 8) {
+      x = 10;
+      y += h + h/2 +10;
+    }
+  }
+}
+
+class Rectangle {
+  constructor(xcord, ycord, width, height) {
+    this.x = xcord;
+    this.y = ycord;
+    this.w = width;
+    this.h = height;
+    this.c = canvas.getContext("2d");
+    this.c.fillStyle = "rgba(255,255,255,.8)";
+    this.c.lineWidth = 4;
+    this.c.strokeStyle = 'pink';
+    this.c.fillRect(this.x, this.y, this.w, this.h);
+    this.c.stroke()
+    this.c.fillStyle = "black";
+    this.c.font = "bold 9pt Verdana";
+    this.c.textAlign = "center";
+    this.text = `[${this.x.toFixed(2)}, ${this.y.toFixed(2)}]`;
+    this.c.fillText(this.text, this.x + this.w / 2, this.y + this.h / 2);
+    this.c.fillStyle = "rgba(255,255,255,.8)";
+  }
+}
